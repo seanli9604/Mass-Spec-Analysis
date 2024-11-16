c25fa32bafe1dfad56fc0e639fafb9c4f6ed0966
cjnbennett <conor.jn.bennett@gmail.com>
2024-11-16 10:04:27 +0800

    db

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.database import engine, get_db
+from app.models import Base#, Item
 
 app = FastAPI()
 
@@ -13,3 +16,16 @@ app.add_middleware(
 @app.get("/")
 def root():
     return {"message": "Hello world!"}
+
+@app.on_event("startup")
+async def startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+# @app.post("/items/")
+# async def create_item(name: str, description: str, db: AsyncSession = Depends(get_db)):
+#     new_item = Item(name=name, description=description)
+#     db.add(new_item)
+#     await db.commit()
+#     await db.refresh(new_item)
+#     return new_item
