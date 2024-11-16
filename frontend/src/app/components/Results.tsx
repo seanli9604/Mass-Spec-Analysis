e17d4864a9a7b1da07e4b1231dbe389f8e4e4f84
cjnbennett <conor.jn.bennett@gmail.com>
2024-11-16 16:01:50 +0800

    resolved merge

--- frontend/src/app/components/Results.tsx
+++ frontend/src/app/components/Results.tsx
@@ -9,9 +9,13 @@ interface ResultsProps {
 export default function Results({ smilesArr }: ResultsProps) {
   return (
     <div className="border border-gray-300 p-4 pb-0 w-1/3 mx-auto mt-8">
-      {smilesArr.map((item, index) => (
-        <ResultBox smiles={item} key={index} />
-      ))}
+      {smilesArr.length > 0 ? (
+        smilesArr.map((item, index) => (
+          <ResultBox key={index} smiles={item} />
+        ))
+      ) : (
+        <p className="text-center pb-4">No results found 🙁</p>
+      )}
     </div>
   );
 }
