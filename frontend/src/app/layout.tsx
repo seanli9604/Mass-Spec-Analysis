d3c7a8b208dcf0dbe6175006e053c80486362a5e
cjnbennett <conor.jn.bennett@gmail.com>
2024-11-17 02:10:06 +0800

    user session

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import NavBar from "./components/NavBar";
+import SessionWrapper from "./components/SessionWrapper";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -7,21 +8,19 @@ export const metadata: Metadata = {
   description: "",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body>
-        <div className="min-h-screen bg-white font-roboto min-w-screen">
-          <NavBar />
+        <SessionWrapper>
+          <div className="min-h-screen bg-white font-roboto min-w-screen">
+            <NavBar />
 
-          <main className="mx-auto px-4 py-10 text-center">
-            {children}
-          </main>
-        </div>
+            <main className="mx-auto px-4 py-10 text-center">
+              {children}
+            </main>
+          </div>
+        </SessionWrapper>
       </body>
     </html>
   );
--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,18 +1,39 @@
 "use client";
 
-import { signIn } from "next-auth/react";
+import { useSession, signIn, signOut } from "next-auth/react";
+import RiseLoader from "react-spinners/RiseLoader";
 
 export default function LoginPage() {
+  const { data: session, status } = useSession();
+
+  console.log(status);
+  
   return (
     <div className="flex justify-center">
       <div className="border border-gray-300 p-4 w-1/3">
         <h3 className="text-2xl text-center font-semibold mb-6">Login</h3>
-        <button
-          onClick={() => signIn("google")}
-          className="block w-full border border-gray-300 p-2 cursor-pointer hover:bg-gray-100 text-center align-middle"
-        >
-          Login with Google
-        </button>
+        {status === "loading" ? (
+          <RiseLoader color="#848484" size="5px" className="mt-6 mb-2" />
+        ) : status === "unauthenticated" ? (
+          <button
+            onClick={() => signIn("google")}
+            className="block w-full border border-gray-300 p-2 cursor-pointer hover:bg-gray-100 text-center align-middle"
+          >
+            Login with Google
+          </button>
+        ) : (
+          <>
+            <p className="mb-4">
+              You are logged in as {session?.user?.name}
+            </p>
+            <button
+              onClick={() => signOut()}
+              className="block w-full border border-gray-300 p-2 cursor-pointer hover:bg-gray-100 text-center align-middle"
+            >
+              Log Out
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
