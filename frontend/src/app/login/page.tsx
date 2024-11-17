1089d78cbfbd6e72080ee6d4b60a7d021ff9c2e6
cjnbennett <conor.jn.bennett@gmail.com>
2024-11-17 10:49:55 +0800

    sign out loading flow

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -6,15 +6,25 @@ import RiseLoader from "react-spinners/RiseLoader";
 
 export default function LoginPage() {
   const { data: session, status } = useSession();
-  const [isLoggingIn, setIsLoggingIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSignIn = async () => {
-    setIsLoggingIn(true);
+    setIsLoading(true);
     try {
       await signIn("google");
     } catch (error) {
       console.error("Sign-in failed", error);
-      setIsLoggingIn(false);
+      setIsLoading(false);
+    }
+  };
+
+  const handleSignOut = async () => {
+    setIsLoading(true);
+    try {
+      await signOut();
+    } catch (error) {
+      console.error("Sign-out failed", error);
+      setIsLoading(false);
     }
   };
 
@@ -24,26 +34,22 @@ export default function LoginPage() {
     <div className="flex justify-center">
       <div className="border border-gray-300 p-4 w-1/3">
         <h3 className="text-2xl text-center font-semibold mb-6">Login</h3>
-        {status === "loading" ? (
+        {(status === "loading" || isLoading) ? (
           <RiseLoader color="#848484" size="5px" className="mt-6 mb-2" />
         ) : status === "unauthenticated" ? (
-          isLoggingIn ? (
-            <RiseLoader color="#848484" size="5px" className="mt-6 mb-2" />
-          ) : (
-            <button
-              onClick={handleSignIn}
-              className="block w-full border border-gray-300 p-2 cursor-pointer hover:bg-gray-100 text-center align-middle"
-            >
-              Login with Google
-            </button>
-          )
+          <button
+            onClick={handleSignIn}
+            className="block w-full border border-gray-300 p-2 cursor-pointer hover:bg-gray-100 text-center align-middle"
+          >
+            Login with Google
+          </button>
         ) : (
           <>
             <p className="mb-4">
               You are logged in as {session?.user?.name}
             </p>
             <button
-              onClick={() => signOut()}
+              onClick={handleSignOut}
               className="block w-full border border-gray-300 p-2 cursor-pointer hover:bg-gray-100 text-center align-middle"
             >
               Log Out
